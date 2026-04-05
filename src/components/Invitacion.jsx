af9da7e4890cf4afa2ad8d8dd484616f0e58c8c8
YeriZ_0 <fredycampos726@gmail.com>
2026-04-05 13:38:02 -0600

    feat(invitation): implementa lienzos de cronograma e información general

--- src/components/Invitacion.jsx
+++ src/components/Invitacion.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { leerInvitadoUnico, actualizarDatos } from '../services/api'
 import { sizeVariable } from '../utils/sizeVariable'
+import { itinerarioActividades } from '../data/dataInvitacion'
 
 import { Lienzo } from './Lienzo'
 import { DecoracionHoja } from './DecoracionHoja'
@@ -11,8 +12,16 @@ import {
     IconoCalendario,
     IconoRuta,
     IconoIglesia,
-    IconoBanquete
-} from '../assets/icon/iconosInvitacion'
+    IconoBanquete,
+    IconoHorario,
+    IconoRegaloDinero,
+    IconoVestido,
+    IconoZapato,
+    IconoUsuarioConfirmado,
+    IconoWhatsapp,
+    IconoCorazon
+} from '../assets/icon/IconosInvitacion'
+import { LineaTemporal } from './LineaTemporal'
 
 import '../styles/invitacion.css'
 
@@ -22,6 +31,13 @@ export const Invitacion = () => {
     const [abierta, setAbierta] = useState(null)
     const { slug } = useParams()
     
+    const estado = (datos) => {
+        if (datos.estado == 'confirmado') return 'confirmado'
+        if (datos.estado == 'cancelado') return 'cancelado'
+
+        return 'pendiente'
+    }
+
     useEffect(() => {
         const traerDatos = async () => {
             try {
@@ -52,7 +68,7 @@ export const Invitacion = () => {
             {/* --- PARTE 1: EL SOBRE / PASTA (Siempre visible o hasta que se abra) --- */}
             {!abierta ? (
                 <div className="fade-in" key={'sobre'}>
-                <Lienzo color="var(--white)" clas>
+                <Lienzo color="var(--carta-color-1)" clas>
                     {/* Decoraciones de la portada */}
                     <DecoracionHoja fila={1} columna={2} size={sizeVariable(200, 25, 250)} top="-100px" left="-80px" rotation={-240} />
                     <DecoracionHoja fila={2} columna={4} size={sizeVariable(180, 25, 220)} bottom="-90px" right="-80px" rotation={-30} />
@@ -65,11 +81,11 @@ export const Invitacion = () => {
                             <div className="pase-seccion">
                                 <p className="txt-details" style={{ marginBottom: '10px' }}>Especialmente para</p>
                                 <h2 className="invitado-nombre-portada">{datos.nombre}</h2>
-                            <div className="asientos-cantidad">
-                                <strong>
-                                    {datos.reserva} {datos.reserva > 1 ? 'PASES RESERVADOS' : 'PASE RESERVADO'}
-                                </strong>
-                            </div>
+                                <div className="asientos-cantidad">
+                                    <strong>
+                                        {datos.reserva} {datos.reserva > 1 ? 'PASES RESERVADOS' : 'PASE RESERVADO'}
+                                    </strong>
+                                </div>
                             </div>
 
                             <button className="btn-abrir" onClick={() => setAbierta(true)}>
@@ -84,10 +100,11 @@ export const Invitacion = () => {
 
                     <Lienzo color="var(--carta-color-1)">
                         <DecoracionHoja fila={1} columna={3} size="150px" top="0" right="0" />
-                        <p className="txt-romantic">" Ya no son dos, sino uno solo. Por tanto, lo que Dios ha unido, que no lo separe el hombre. " (Mateo 19:6)</p>
+                        <p className="txt-romantic" style={{marginBottom: 0}}>" Ya no son dos, sino uno solo.</p>
+                        <p className="txt-romantic" style={{marginTop: 0}}>Por tanto, lo que Dios ha unido, que no lo separe el hombre. " (Mateo 19:6)</p>
         
                         <div className="novios-nombres" style={{ padding: '20px 0'}}>
-                            Kenny <span className="novios-separador">&</span> Mauricio
+                            Kenny <div className='icon' style={{ paddingTop: '20px'}}> <IconoCorazon size='70'/> </div> Mauricio
                         </div>
                         <div className='txt-details conteiner'>
                             <p>Dale</p>
@@ -129,7 +146,7 @@ export const Invitacion = () => {
                     <Lienzo color="var(--carta-color-1)">
                         
                         <div className='informacion-actividad'>
-                            <div className='icon'> <IconoIglesia size='30' /> </div>
+                            <div className='icon'> <IconoIglesia size='55' /> </div>
                             <p><strong>Ceremonia Religiosa</strong></p>
                         </div>
                         <p className='label-mapa'>
@@ -151,14 +168,14 @@ export const Invitacion = () => {
                             target="_blank" 
                             rel="noopener noreferrer"
                             className='btn-mapa'>
-                            <div className='icon'> <IconoRuta size='25' /> </div>
+                            <div className='icon'> <IconoRuta size='30' /> </div>
                             <p>Ver trayecto en <strong>Waze</strong></p>
                         </a>
                         
                         <div className='separator' />
 
                         <div className='informacion-actividad'>
-                            <div className='icon'> <IconoBanquete size='30' /> </div>
+                            <div className='icon'> <IconoBanquete size='55' /> </div>
                             <p><strong>Recepción</strong></p>
                         </div>
                         <p className='label-mapa'>
@@ -185,24 +202,101 @@ export const Invitacion = () => {
                         </a>
 
                     </Lienzo>
+                    
+                    <Lienzo color='var(--carta-color-2)'>
+                        <div className='informacion-actividad' style={{ color: 'var(--text-lienzo-accent-2)' }}>
+                            <div className='icon'>
+                                <IconoHorario size='75' /> 
+                            </div>
+                            <p><strong>Itinerario</strong></p>
+                        </div>    
 
-                    {/* Aquí irán las secciones de Mapa, Música, Vestimenta y Confirmación */}
-                    <Lienzo color="var(--white)">
-                        <p>Sección de Mapa y demás detalles en construcción...</p>
-                        {/* Botones de confirmación al final de todo el recorrido */}
-                        <button className="btn-abrir" onClick={() => setAbierta(false)}>
-                        Cerrar Invitación
-                        </button>
+                        <p className='txt-details' style={{ color: 'var(--text-lienzo-accent-2)' }} >
+                            El día de nuestra boda.
+                        </p>                    
+
+                        <LineaTemporal data={itinerarioActividades}></LineaTemporal>
+                    </Lienzo>
+
+                    <Lienzo color='var(--carta-color-1)'>
+                        <div className='informacion-actividad'>
+                            <div className='icon'>
+                                <IconoVestido size='50'/>
+                                <IconoZapato size='50' />
+                            </div>
+                            <p><strong>Vestimenta</strong></p>   
+                        </div> 
+
+                        <p className='label-mapa'>
+                            Para honrar la integridad del estilo nupcial y el protagonismo de la novia, solicitamos a nuestras invitadas asistir con gala formal, <strong>omitiendo el uso de prendas blancas o niveas</strong> durante el evento.
+                        </p>
+
+                        <div className='separator' style={{paddingTop: '30px'}}/>
+
+                        <div className='informacion-actividad'>
+                            <div className='icon'>
+                                <IconoRegaloDinero size='75'/>
+                            </div>
+                            <p><strong>Sugerencia de Regalos</strong></p>   
+                        </div> 
+
+                        <p className='label-mapa'>
+                            Su presencia es el mayor obsequio que podemos recibir. No obstante, si desea tener un detalle con nosotros, agradeceremos su muestra de afecto mediante un sobre el dia de nuestro enlace.
+                        </p> 
+                    </Lienzo>
+
+                    <Lienzo color='var(--carta-color-2)'>
+                        <div className='informacion-actividad' style={{ color: 'var(--text-lienzo-accent-2)' }}>
+                            <div className='icon'>
+                                <IconoUsuarioConfirmado size='50' /> 
+                            </div>
+                            <p><strong>Confirmar Asistencia</strong></p>
+                        </div>    
+
+                        <p className='label-mapa' style={{ color: 'var(--text-lienzo-accent-2)' }}>
+                            Agradecemos que confirmes tu asistencia <strong>antes del 10 de mayo de 2026</strong>
+                        </p>
+
+                        <div className='confirmar-container'>
+                            <h2 className="invitado-nombre-portada">{datos.nombre}</h2>
+                            <div className="asientos-cantidad">
+                                <strong>
+                                    {datos.reserva} {datos.reserva > 1 ? 'PASES RESERVADOS' : 'PASE RESERVADO'}
+                                </strong>
+                            </div>
+
+                            <p className='label-mapa'>
+                                Su respuesta a la invitación es
+                            </p>
+
+                            <p className={`estado-invitado ${estado(datos)}`} style={{ color: 'var(--text-lienzo-accent-2)' }} >
+                                {datos.estado}
+                            </p>  
+                        </div>
+
+                        <div className="acciones">
+                            <button onClick={() => confirmar('confirmado')}>Confirmar Asistencia</button>
+                            <button onClick={() => confirmar('cancelado')}>No podre asistir</button>
+                        </div>
+                    </Lienzo>
+
+                    <Lienzo color="var(--carta-color-1)">
+                        <div className="final-agradecimiento">
+                            <p className="texto-despedida">
+                                <div className='icon'> <IconoCorazon size='70'/> </div>
+                                Esperamos contar con su presencia
+                            </p>
+
+                            <button 
+                                className="btn-cerrar-vista"
+                                onClick={() => setAbierta(false)} 
+                            >
+                                Finalizar
+                            </button>
+                        </div>
                     </Lienzo>
                 </div>
             )}
-            
-            {/* <div className="acciones">
-                <button onClick={() => confirmar('confirmado')}>Confirmar Asistencia</button>
-                <button onClick={() => confirmar('cancelado')}>No podre asistir</button>
-            </div> */}
-        </main>
-
-            
+        </main>  
     )
 }
